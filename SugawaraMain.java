d72ae65fadf214d473ab31def80d1c7b473d5de4
pommebarque-megumi <cache2gecko@gmail.com>
2022-03-24 14:09:20 +0900

    月の日数表示

---
+++ SugawaraMain.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class SugawaraMain{
+	public static void main(String[] args){
+		System.out.print("月を入力してください。>>");
+		int month = new Scanner(System.in).nextInt();
+		switch(month){
+			case 1:
+			case 3:
+			case 5:
+			case 7:
+			case 8:
+			case 10:
+			case 12:
+				System.out.println(month+"月は31日まであります。");
+				break;
+			case 2:
+				System.out.println(month+"月は28日まであります。");
+				break;
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				System.out.println(month+"月は30日まであります。");
+				break;
+			default:
+				System.out.println("そんな月はありません。");
+		}
+	}
+}
